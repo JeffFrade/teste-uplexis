67a0e2100c14244f494e90737adf22fe3551fcce
Jefferson Frade <jefferrson.frade@gmail.com>
2019-05-29 23:44:06 -0300

    Created tables and changes on UserFactory

---
+++ database/migrations/2019_05_30_024039_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artigos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo', 300);
+            $table->string('link', 500);
+            $table->integer('id_usuario');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
