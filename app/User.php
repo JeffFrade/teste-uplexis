b3a5c2ecba48554a41b01e269adf67cc85edbffe
Jefferson Frade <jefferrson.frade@gmail.com>
2019-05-29 23:47:30 -0300

    Finished tables, factories and seeders

--- app/User.php
+++ app/User.php
@@ -2,38 +2,20 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $table = 'usuario';
     protected $fillable = [
-        'name', 'email', 'password',
+        'usuario', 'senha',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'senha'
     ];
 }
